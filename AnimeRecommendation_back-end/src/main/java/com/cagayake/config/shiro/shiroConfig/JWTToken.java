3b32dbec3ba9d2fd2b28406e9a5a6a8187b96c3a
Zhekai Zhang <zzhang5@bsu.edu>
2020-06-24 02:05:01 -0400

    Removed some meaningless comments

--- AnimeRecommendation_back-end/src/main/java/com/cagayake/config/shiro/shiroConfig/JWTToken.java
+++ AnimeRecommendation_back-end/src/main/java/com/cagayake/config/shiro/shiroConfig/JWTToken.java
@@ -5,7 +5,6 @@ import org.apache.shiro.authc.AuthenticationToken;
 
 public class JWTToken implements AuthenticationToken {
 
-    // 密钥
     private String token;
 
     public JWTToken(String token) {
